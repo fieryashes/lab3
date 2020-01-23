786a787b0bf66a0d14acd1dbb5b21c6552089ca2
Alisha Sehgal <alishaseh@gmail.com>
2020-01-23 13:18:42 -0800

    heroku time

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -10,9 +10,39 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript is ready to roll");
+		$("#testjs").text("Please wait...")
+
 	});
+
+	$("a.thumbnail").click(projectClick);
+	$(".jumbotron p").addClass("active");
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	// prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    // $(this).css("background-color", "#7fff00");
+
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+	
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).fadeToggle()
+       // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+
+
+	// // prevent the page from reloading      
+ //    e.preventDefault();
+
 }
